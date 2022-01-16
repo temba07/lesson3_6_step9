a2032da420fed85c4c1918ef357774025670f001
Tembulat_Ketov <tketov@list.ru>
2022-01-16 21:48:49 +0300

    no message

--- conftest.py
+++ conftest.py
@@ -1,16 +1,16 @@
-from selenium.webdriver.chrome.options import Options
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.add_experimental_option('prefs', {'intl.accept_languages': 'es'})
-browser = webdriver.Chrome(options=options)
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None, help="Choose language")
 
-
-@pytest.fixture()
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
--- lesson3_6_step9/test_items.py
+++ lesson3_6_step9/test_items.py
@@ -1,9 +1,12 @@
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_lesson3_6_step9(browser):
+def test_btn_add_to_shopping_cart(browser):
     browser.get(link)
-    browser.implicitly_wait(10)
-    button = browser.find_element_by_css_selector("[class = 'btn btn-lg btn-primary btn-add-to-basket']")
-    assert button == True, "Кнопка не найдена"
-    
+    button = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-add-to-basket")))
+    assert button, "Кнопка отсутствует"
+    time.sleep(10)
